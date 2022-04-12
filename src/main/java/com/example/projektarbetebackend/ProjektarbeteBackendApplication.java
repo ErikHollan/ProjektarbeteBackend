93695ca1cd56d5769450d03724b01570e655e6f2
Erik Hollander <erik.hollander@yh.nackademin.se>
2022-04-12 10:33:20 +0200

    Initial commit

---
+++ src/main/java/com/example/projektarbetebackend/ProjektarbeteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.projektarbetebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektarbeteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektarbeteBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projektarbetebackend/ProjektarbeteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projektarbetebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektarbeteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
