8c10a76e6591919c0dc22fffa26059eff20f573e
Rothoff <92538526+Rothoff@users.noreply.github.com>
2022-04-21 21:45:18 +0200

    Added 3 test-classes för all the controllers

---
+++ src/test/java/com/example/projektarbetebackend/Controllers/ItemControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.projektarbetebackend.Controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ItemControllerTest {
+
+    @Test
+    void addNewProduct() {
+    }
+
+    @Test
+    void newOrder() {
+    }
+
+    @Test
+    void getAllProducts() {
+    }
+
+    @Test
+    void getitemById() {
+    }
+}
