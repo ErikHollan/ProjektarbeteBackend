8eb18e8ce62e20ea59943ae85779815f80e4240d
JohanssonJacob <jacobjohanssn2001@gmail.com>
2022-05-26 14:27:06 +0200

    Added frontend as module

---
+++ Frontend/src/main/java/com/example/frontendprojektarbetebackend/FrontendProjektarbeteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.frontendprojektarbetebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendProjektarbeteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendProjektarbeteBackendApplication.class, args);
+    }
+
+}
